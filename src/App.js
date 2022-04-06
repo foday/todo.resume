4cc7a3e26fca703d2b58caa59c052b68aacd1005
foday <foday_s@yahoo.com>
2022-04-06 10:41:29 -0500

    Html & jsx setup, TodoList component created
    Start by adding the html elements to App.js and creating the <TodoList/> component then importing it into the App.js file. The html and TodoList component are all wrapped in a open and closed <div> element. Components are functions that return jsx. They also make up the visual layer of our application and allow the dev to split the application into different pieces

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-
-import './App.css';
+import TodoList from './TodoList';
 
 function App() {
   return (
-    <div className="App">
-     <h1>Hello world</h1>
+    <div>
+     <TodoList/>
+     <input type="text" />
+     <button>Add todo</button>
+     <button>Clear</button>
+     <div>0 left to do</div>
     </div>
   );
 }
